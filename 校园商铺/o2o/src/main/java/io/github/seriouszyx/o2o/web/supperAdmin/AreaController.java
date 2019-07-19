07ff38f4d126dd26462084f4987e25c6c9f06db5
seriouszyx <674965440@qq.com>
2019-07-19 08:35:38 +0800

    校园商铺 - 配置logback

--- 校园商铺/o2o/src/main/java/io/github/seriouszyx/o2o/web/supperAdmin/AreaController.java
+++ 校园商铺/o2o/src/main/java/io/github/seriouszyx/o2o/web/supperAdmin/AreaController.java
@@ -2,6 +2,8 @@ package io.github.seriouszyx.o2o.web.supperAdmin;
 
 import io.github.seriouszyx.o2o.entity.Area;
 import io.github.seriouszyx.o2o.service.AreaService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +23,8 @@ import java.util.Map;
 @Controller
 @RequestMapping("/superadmin")
 public class AreaController {
+
+    Logger logger = LoggerFactory.getLogger(AreaController.class);
 
     @Autowired
     private AreaService areaService;
